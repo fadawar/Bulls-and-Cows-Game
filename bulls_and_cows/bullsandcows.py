0f209d39bc7f3231bb31850f255aff7f3f8bb9d1
fadawar <fadawar@gmail.com>
2017-02-11 22:38:15 +0100

    Add support for different number size

--- bulls_and_cows/bullsandcows.py
+++ bulls_and_cows/bullsandcows.py
@@ -5,23 +5,24 @@ import string
 class Game:
     """Bulls and Cows game"""
 
-    def __init__(self, input_func):
+    def __init__(self, input_func, number_size):
         self._input_func = input_func
-        self._secret_number = ''.join(random.sample(string.digits, 4))
+        self._number_size = number_size
+        self._secret_number = ''.join(random.sample(string.digits, number_size))
         self._guesses = 0
 
     def play(self):
         print("Hi there!")
-        print("I've generated a random 4 digit number for you.")
+        print("I've generated a random {} digit number for you.".format(self._number_size))
         print("Let's play a bulls and cows game.")
-        self.get_and_check_answers()
+        self.ask_and_evaluate_answers()
         self.print_results()
 
-    def get_and_check_answers(self):
+    def ask_and_evaluate_answers(self):
         while True:
             try:
                 self._guesses += 1
-                number = self.ask_for_number()
+                number = self.get_guess()
                 if number == self._secret_number:
                     break       # correct guess, stop asking
                 else:
@@ -31,11 +32,11 @@ class Game:
                 print(e)
                 continue        # go and ask again
 
-    def ask_for_number(self):
+    def get_guess(self):
         print('Enter a number')
         input_ = self._input_func().strip()
-        if not input_.isdecimal() or len(set(input_)) != 4:
-            raise WrongInputException('Wrong input. Input must be 4 different digits.')
+        if not input_.isdecimal() or len(set(input_)) != self._number_size:
+            raise WrongInputException('Wrong input. Input must be {} different digits.'.format(self._number_size))
         return input_
 
     def print_results(self):
@@ -56,7 +57,7 @@ class BullsCowsCount:
         self._count(secret, guess)
 
     def _count(self, secret, guess):
-        for i in range(4):
+        for i in range(len(secret)):
             if secret[i] == guess[i]:
                 self._bulls += 1
             elif guess[i] in secret:
@@ -78,4 +79,4 @@ class WrongInputException(Exception):
 
 
 if __name__ == '__main__':
-    Game(input).play()
+    Game(input, 4).play()
--- tests/test_game.py
+++ tests/test_game.py
@@ -3,27 +3,27 @@ from bulls_and_cows.bullsandcows import Game, BullsCowsCount, WrongInputExceptio
 
 
 def test_game_has_secret_number():
-    g = Game(lambda: 0)
+    g = Game(lambda: 0, 4)
     assert g._secret_number is not None
     assert type(g._secret_number) is str
 
 
 def test_secret_number_has_4_digits():
-    g = Game(lambda: 0)
+    g = Game(lambda: 0, 4)
     assert len(g._secret_number) == 4
 
 
 def test_secret_numbers_digits_are_different():
-    g = Game(lambda: 0)
+    g = Game(lambda: 0, 4)
     s = set(g._secret_number)
     assert len(s) == 4
 
 
 def test_ask_until_correct_answer():
     input_generator = (i for i in ['2016', '2017'])
-    g = Game(lambda: next(input_generator))
+    g = Game(lambda: next(input_generator), 4)
     g._secret_number = '2017'
-    g.get_and_check_answers()
+    g.ask_and_evaluate_answers()
     assert g._guesses == 2
 
 
@@ -40,28 +40,28 @@ def test_count_of_cows():
     assert BullsCowsCount('1234', '3419').cows() == '3 cows'
 
 
-def test_ask_for_number_returns_input():
-    g = Game(lambda: '1234')
-    assert g.ask_for_number() == '1234'
+def test_get_guess_returns_input():
+    g = Game(lambda: '1234', 4)
+    assert g.get_guess() == '1234'
 
 
-def test_ask_for_number_checks_input():
-    g1 = Game(lambda: '12')                                     # too short
+def test_get_guess_checks_input():
+    g1 = Game(lambda: '12', 4)                                     # too short
     with pytest.raises(WrongInputException) as exc_info:
-        g1.ask_for_number()
+        g1.get_guess()
         assert str(exc_info.value) == 'Wrong input. Input must be 4 different digits.'
 
-    g2 = Game(lambda: '123456')                                 # too long
+    g2 = Game(lambda: '123456', 4)                                 # too long
     with pytest.raises(WrongInputException) as exc_info:
-        g2.ask_for_number()
+        g2.get_guess()
         assert str(exc_info.value) == 'Wrong input. Input must be 4 different digits.'
 
-    g3 = Game(lambda: '12ab')                                   # not a number
+    g3 = Game(lambda: '12ab', 4)                                   # not a number
     with pytest.raises(WrongInputException) as exc_info:
-        g3.ask_for_number()
+        g3.get_guess()
         assert str(exc_info.value) == 'Wrong input. Input must be 4 different digits.'
 
-    g4 = Game(lambda: '1223')                                   # same digits
+    g4 = Game(lambda: '1223', 4)                                   # same digits
     with pytest.raises(WrongInputException) as exc_info:
-        g4.ask_for_number()
+        g4.get_guess()
         assert str(exc_info.value) == 'Wrong input. Input must be 4 different digits.'
